385df8055619c78e14ca585efd1c19238f3ab1f3
Lukasz Chwiedosik <lukasz.chwiedosik@gmail.com>
2021-11-10 18:10:39 +0100

    Update task

---
+++ task_3.js
@@ -0,0 +1,49 @@
+//  task 1
+
+let iffeFunction = (function(){
+    let privVar = "Zmienna prywatna typu string"
+
+    function showValue(){
+        if(privVar == null || privVar == undefined){
+            return `Zmienna prywatna jest null lub undefined w tym przypadku: ${privVar}`
+        }else{
+            return `Wartość zmiennej: ${privVar}`
+        }
+    }
+
+    function setValue(newValue){
+        if(newValue){
+            privVar = newValue
+        }
+    }
+
+    function reverseValue(){
+        if(Number.isInteger(privVar) == true){
+            return "Zmiana wartości: " + (privVar * (-1))
+        }else if(typeof privVar == "string"){
+            return `Odwrócona wartość: ${privVar.split('').reverse().join('')}`
+        }else{
+            return "Zmienna jest null lub undefined"
+        }
+    }
+
+    return{
+        showValue,
+        setValue,
+        reverseValue
+    }
+
+})()
+
+console.log(iffeFunction.showValue())
+console.log(iffeFunction.reverseValue())
+iffeFunction.setValue(12345)
+console.log(iffeFunction.showValue())
+console.log(iffeFunction.reverseValue())
+iffeFunction.setValue("")
+console.log(iffeFunction.showValue())
+console.log(iffeFunction.reverseValue())
+
+
+
+
