2706d74a1824e5d729c8ad8e82b3e241c21e86ad
Lukasz Chwiedosik <lukasz.chwiedosik@gmail.com>
2021-11-10 21:40:16 +0100

    Update task

--- task_3.js
+++ task_3.js
@@ -41,6 +41,56 @@
 // console.log(iffeFunction.showValue())
 // console.log(iffeFunction.reverseValue())
 
+//  task 2
+
+function sum(x,y){
+
+}
+
+function sub(x,y){
+
+}
+
+function mul(x,y){
+
+}
+
+function div(x,y){
+
+}
+
+function aha(){
+    let object = {
+        x: 10,
+        y: 15,
+        getFunction: sum,
+    }
+    return object
+}
+
+//  task 3
+
+let BaseObject = {
+    x: 0,
+    y: 0,
+    sum: function(){
+        return this.x + this.y
+    }
+}
+let objectArray = {x:2,y:3}
 
 
+// for(let i = 0; i < objectArray.length; i++){
+//     console.log(BaseObject.sum.call(objectArray[i]))
+// }
+// console.log(BaseObject.sum.call(objectArray[1]))
+console.log(BaseObject.sum.bind(objectArray[1]))
+let lol =BaseObject.sum.bind(objectArray)
+console.log(lol())
+
+
+
+
+// console.log(BaseObject.sum())
+// console.log(objectArray[0])
 
