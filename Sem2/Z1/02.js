4458606df23c1557cc970f0dbe3002384934ef27
Lukasz Chwiedosik <lukasz.chwiedosik@gmail.com>
2022-03-18 22:50:02 +0100

    New version of task number 02

--- Sem2/Z1/02.js
+++ Sem2/Z1/02.js
@@ -1,40 +1,72 @@
 let startPoint = 1;
 let endPoint = 100;
-let count = 0;
+class UserNumber{
 
-const computerNumber = randomNumber(startPoint, endPoint)
-let userNumber = randomNumber(startPoint, endPoint)
+    static createNumber(startPoint, endPoint){
 
-console.log(computerNumber, 'comp')
-console.log(userNumber, 'User')
+        // console.log(startPoint, endPoint, 'Start i end')
+        let number = Math.floor(Math.random() * (endPoint - startPoint) + startPoint);
+        return number;
+    }
 
-while(computerNumber !== userNumber){
-    
-    if(computerNumber > userNumber){
-        console.log(`Your number ${userNumber} is too small!`)
-        count += 1;
-        startPoint = userNumber;
-        // console.log(`new Start ${startPoint}`)
-        userNumber = randomNumber(startPoint, endPoint)
-    }else if(computerNumber < userNumber){
-        console.log(`Your number ${userNumber} is too big!`)
-        count += 1;
-        endPoint = userNumber;
-        // console.log(`new end ${endPoint}`)
-        userNumber = randomNumber(startPoint, endPoint)
+    static onlyUniqueNumbers(numbersArray){
+        let unique = numbersArray.filter((element, index, arr) => {
+            return arr.indexOf(element) === index
+        })
+        return unique
+    }
+
+    static compareNumbers(computerNumber, startPoint, endPoint){
+
+        let numbersArray = new Array();
+        let userNumber = UserNumber.createNumber(startPoint,endPoint);
+        numbersArray.push(userNumber)
+
+        let newStartPoint = startPoint;
+        let newEndPoint = endPoint;
+        
+        console.log('Computer number', computerNumber)
+
+        while(computerNumber !== userNumber){
+
+            if(computerNumber > userNumber){
+                console.log(`Your number ${userNumber} is too small!`)
+                newStartPoint = userNumber;
+                userNumber = UserNumber.createNumber(newStartPoint, newEndPoint);
+                numbersArray.push(userNumber);
+
+            }else if(computerNumber < userNumber){
+                console.log(`Your number ${userNumber} is too big!`)
+                newEndPoint = userNumber;
+                userNumber = UserNumber.createNumber(newStartPoint, newEndPoint);
+                numbersArray.push(userNumber);
+
+            }
+
+            if(computerNumber === userNumber){
+                
+                console.log(`You guessed computer number. That number is: ${userNumber}`);
+                console.log(`Yours all numbers: ${UserNumber.onlyUniqueNumbers(numbersArray)}`)
+            }
+
+        }
     }
 }
 
-if(computerNumber === userNumber){
-    count += 1;
-    console.log(`Numbers are the same! ${computerNumber} = ${userNumber}`)
-    console.log(`Value of counter: ${count}`)
+class ComputerNumber{
+
+    constructor(startPoint,endPoint){
+        this.startPoint = startPoint;
+        this.endPoint = endPoint;
+    }
+
+    createNumber(){
+        let number = Math.floor(Math.random() * (this.endPoint - this.startPoint) + this.startPoint);
+        return number;
+    }
 }
 
-function randomNumber(start, end){
-    let randomNumber = Math.floor(Math.random() * (end - start) + start)
-    // console.log(`start from f ${start} end from f ${end}`)
-    return randomNumber
-}
 
+let computerNumber = new ComputerNumber(startPoint,endPoint)
 
+UserNumber.compareNumbers(computerNumber.createNumber(), startPoint, endPoint)
